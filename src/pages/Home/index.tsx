1f1c6291085bb496d3a32f8c78df673855199e15
Geng Yuan <mengmiao.0101@icloud.com>
2021-08-30 16:13:04 +0800

    📦 封装面包屑导航，增加product路由

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+interface Props {}
+
+export default function index({}: Props): ReactElement {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
+}
