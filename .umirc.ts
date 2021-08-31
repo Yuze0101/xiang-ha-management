07bebcdc7e388a75bb100b869e54617b9ce16bae
leihao <leihao@gmail.com>
2021-08-31 14:12:33 +0800

    更新路由

--- .umirc.ts
+++ .umirc.ts
@@ -10,8 +10,18 @@ export default defineConfig({
             component: '@/pages/index',
             routes: [
                 { path: '/', component: '@/pages/Home/index' },
-                { path: '/product', component: '@/pages/Product/index' },
+                {
+                    path: '/product',
+                    component: '@/pages/Product/index',
+                    routes: [
+                        {
+                            path: '/product/addproduct',
+                            component: '@/pages/Product/addProduct',
+                        },
+                    ],
+                },
                 { path: '/users', component: '@/pages/Users/index' },
+                { path: '/order', component: '@/pages/Order/index' },
             ],
         },
         { path: '/login', component: '@/pages/Login' },
