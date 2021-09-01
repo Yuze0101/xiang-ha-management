e0f73acc394d57895a9308deb86b617047efa69d
Geng Yuan <mengmiao.0101@icloud.com>
2021-09-01 10:53:03 +0800

    📦 完成管理员页面 重新封装formmodal

--- src/apis/admin.ts
+++ src/apis/admin.ts
@@ -16,6 +16,27 @@ export const login = (data: loginType) => {
 export const getAllAdmins = () => {
     return request.get('/admin/getAllAdmins');
 };
-export const foo3 = (params: {}) => {
-    return request.get('/url/zzzz', { params });
+type addType = {
+    account: string;
+    password: string;
+    realname: string;
+    telephone: string;
+};
+/**
+ * @param {addType} data
+ * @param {string} data.account 登录的账户名
+ * @param {string} data.password 登录密码
+ * @param {string} data.realname 用户的真实名字，用于登录后显示
+ * @param {string} data.telephone 手机号码
+ * @description 添加管理员
+ */
+export const addAdmins = (data: addType) => {
+    return request.post('/admin/addAdmins', { data });
+};
+/**
+ * @param {string} _id 管理员id
+ * @description 删除管理员
+ */
+export const delAdmins = (data: { _id: string }) => {
+    return request.post('/admin/delAdmins', { data });
 };
--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,6 +1,8 @@
-import React, { ReactElement, useState, useEffect } from 'react';
-import { Card, Table, Button, message } from 'antd';
-import { getAllAdmins } from '@/apis/admin';
+import React, { ReactElement, useState, useEffect, useRef } from 'react';
+import { Card, Table, Button, message, Space, Popconfirm } from 'antd';
+import FormModal from '@/components/FormModal/FormModal';
+
+import { getAllAdmins, addAdmins, delAdmins } from '@/apis/admin';
 interface Props {}
 type Admin = {
     _id: string;
@@ -11,28 +13,62 @@ type Admin = {
     telephone: string;
     status: number;
 };
-const columns = [
-    {
-        title: '账户名',
-        dataIndex: 'account',
-        key: 'account',
-        render: (text: string, item: Admin) => <div key={item.key}>{text}</div>,
-    },
-    {
-        title: '联系方式',
-        dataIndex: 'telephone',
-        key: 'telephone',
-        render: (text: string, item: Admin) => <div key={item.key}>{text}</div>,
-    },
-    {
-        title: '操作',
-        key: 'action',
-        render: () => <Button type="primary">编辑</Button>,
-    },
-];
 
 export default function index({}: Props): ReactElement {
+    const [formItem, setFormItem] = useState<any>([]);
+    const [modalTitle, setModalTitle] = useState<string>('添加管理员');
+    const formModalRef = useRef<any>(null);
+    const columns = [
+        {
+            title: '账户名',
+            dataIndex: 'account',
+            key: 'account',
+            render: (text: string, item: Admin) => (
+                <div key={item.key}>{text}</div>
+            ),
+        },
+        {
+            title: '联系方式',
+            dataIndex: 'telephone',
+            key: 'telephone',
+            render: (text: string, item: Admin) => (
+                <div key={item.key}>{text}</div>
+            ),
+        },
+        {
+            title: '真实姓名',
+            dataIndex: 'realname',
+            key: 'realname',
+            render: (text: string, item: Admin) => (
+                <div key={item.key}>{text}</div>
+            ),
+        },
+        {
+            title: '操作',
+            dataIndex: '_id',
+            key: '_id',
+            render: (_id: string) => (
+                <Space size="middle">
+                    <Button type="primary">编辑</Button>
+                    <Popconfirm
+                        placement="top"
+                        onConfirm={() => {
+                            handleConfirm(_id);
+                        }}
+                        title="您确定吗？此操作不可逆"
+                        okText="确认"
+                        cancelText="取消"
+                    >
+                        <Button data-type="delete" type="primary">
+                            删除
+                        </Button>
+                    </Popconfirm>
+                </Space>
+            ),
+        },
+    ];
     const [adminList, setAdminList] = useState([]);
+    // NOTE 异步请求
     const getAdminList = async () => {
         const res = await getAllAdmins();
         if (res.code === 200) {
@@ -46,12 +82,62 @@ export default function index({}: Props): ReactElement {
             message.error('获取管理员列表失败');
         }
     };
+    // NOTE handle 部分
+    const handleAdd = () => {
+        setFormItem([
+            {
+                label: '用户名',
+                name: 'account',
+                rules: [{ required: true, message: '请输入用户名' }],
+            },
+            {
+                label: '密码',
+                name: 'password',
+                rules: [{ required: true, message: '请输入密码' }],
+            },
+            {
+                label: '真实姓名',
+                name: 'realname',
+                rules: [{ required: true, message: '请输入姓名' }],
+            },
+            {
+                label: '手机号码',
+                name: 'telephone',
+                rules: [{ required: true, message: '请输入手机号' }],
+            },
+        ]);
+        formModalRef.current.showModal();
+    };
     useEffect(() => {
         getAdminList();
     }, []);
+    const handleConfirm = async (_id: string) => {
+        const res = await delAdmins({ _id });
+        if (res.code === 200) {
+            message.success('删除成功');
+            getAdminList();
+        } else {
+            message.error('删除失败');
+        }
+    };
+    const cardTitle = (
+        <Space style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div>管理员列表</div>
+            <Button type="primary" onClick={handleAdd}>
+                添加管理员
+            </Button>
+        </Space>
+    );
     return (
-        <Card title="管理员列表">
+        <Card title={cardTitle}>
             <Table columns={columns} dataSource={adminList} />
+            <FormModal
+                title={modalTitle}
+                formItem={formItem}
+                ref={formModalRef}
+                action={addAdmins}
+                parentAction={getAdminList}
+            ></FormModal>
         </Card>
     );
 }
